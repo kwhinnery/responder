99a2efddd1b9fca51f3534a8a3def974b9dba5bd
Kevin Whinnery <kevin.whinnery@gmail.com>
2014-02-06 13:23:37 -0600

    unprotect voice URL

--- controllers/voice.js
+++ controllers/voice.js
@@ -2,18 +2,14 @@ var twilio = require('twilio');
 
 // Express route handler
 module.exports = function(request, response) {
-    if (request.param('secret') !== process.env.TWILIO_SECRET) {
-        response.send(403, 'You are not Twilio... >:/');
-    } else {
-        // Setup TwiML response
-        var twiml = new twilio.TwimlResponse();
+    // Setup TwiML response
+    var twiml = new twilio.TwimlResponse();
 
-        // Send back the proper TwiML
-        var twiml = new twilio.TwimlResponse();
-        twiml.say('Please send an SMS text message to this number to receive information on TB medication. Thank you!', {
-            voice:'woman'
-        });
-        response.type('text/xml');
-        response.send(twiml.toString());
-    }
+    // Send back the proper TwiML
+    var twiml = new twilio.TwimlResponse();
+    twiml.say('Please send an SMS text message to this number to receive information on TB medication. Thank you!', {
+        voice:'woman'
+    });
+    response.type('text/xml');
+    response.send(twiml.toString());
 };
